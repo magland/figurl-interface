92c601e58db8f3e09be23e147d0887bed6765b7e
Jeremy Magland <jmagland@flatironinstitute.org>
2022-12-22 14:42:51 -0500

    parent origin

--- src/sendMessageToParent.ts
+++ src/sendMessageToParent.ts
@@ -5,11 +5,15 @@ const queryParams = Object.fromEntries(urlSearchParams.entries())
 
 const sendMessageToParent = (x: MessageToParent) => {
     if (!queryParams.parentOrigin) {
-        console.warn('No parent origin. Posting message to self.')
+        console.warn('No parent origin. Using own origin')
+    }
+    if (!window.parent) {
+        console.warn('No parent. Posting to self')
         window.postMessage(x, '*')
         return
     }
-    ;(window.parent as any).postMessage(x, queryParams.parentOrigin)
+    const parentOrigin = queryParams.parentOrigin || (window.location.protocol + '//' + window.location.host)
+    ;(window.parent as any).postMessage(x, parentOrigin)
 }
 
 export default sendMessageToParent
