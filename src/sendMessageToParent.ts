97e636ab031b1a146d5b177c5289826be3909474
Jeremy Magland <jmagland@flatironinstitute.org>
2023-02-17 10:29:00 -0500

    post message to opener if no parent

--- src/sendMessageToParent.ts
+++ src/sendMessageToParent.ts
@@ -1,12 +1,13 @@
 import { MessageToParent } from "./viewInterface/MessageToParentTypes";
 
 const sendMessageToParent = (x: MessageToParent, {parentOrigin}: {parentOrigin: string}) => {
-    if (!window.parent) {
-        console.warn('No parent. Posting to self')
+    const parentOrOpenerWindow = window.parent || window.opener
+    if (!parentOrOpenerWindow) {
+        console.warn('No parent or opener. Posting to self')
         window.postMessage(x, '*')
         return
     }
-    ;(window.parent as any).postMessage(x, parentOrigin)
+    parentOrOpenerWindow.postMessage(x, parentOrigin)
 }
 
 export default sendMessageToParent
