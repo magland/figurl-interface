c6d33da44316d103308c3f91cb1d5071df8f0a48
Jeremy Magland <jmagland@flatironinstitute.org>
2023-03-10 17:09:39 -0500

    add readDir

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export {default as startListeningToParent } from './startListeningToParent'
 
 export { default as getFigureData } from './getFigureData'
 export { default as getFileData, getFileDataUrl, useFileData, storeFileData, storeGithubFileData } from './getFileData'
+export { default as readDir } from './readDir'
 export { default as SetupUrlState } from './SetupUrlState'
 export { useUrlState } from './UrlStateContext'
 export { default as useSignedIn } from './useSignedIn'
