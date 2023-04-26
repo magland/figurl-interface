813ec1b328936193f3e3338fa34104c5fc5c260e
Jeremy Magland <jmagland@flatironinstitute.org>
2023-04-26 08:31:26 -0400

    includeUserId

--- src/serviceQuery.ts
+++ src/serviceQuery.ts
@@ -1,11 +1,14 @@
 import sendRequestToParent from "./sendRequestToParent"
 import { isServiceQueryResponse, ServiceQueryRequest } from "./viewInterface/FigurlRequestTypes"
 
-const serviceQuery = async (serviceName: string, query: any): Promise<{result: any, binaryPayload: ArrayBuffer}> => {
+const serviceQuery = async (serviceName: string, query: any, o: {includeUserId?: boolean}={}): Promise<{result: any, binaryPayload: ArrayBuffer}> => {
     const request: ServiceQueryRequest = {
         type: 'serviceQuery',
         serviceName,
         query
+    }
+    if (o.includeUserId) {
+        request.includeUserId = true
     }
     const response = await sendRequestToParent(request)
     if (!isServiceQueryResponse(response)) throw Error('Invalid response to serviceQuery')
